422c3144b5d63b47f99dc9fd9f0affad9fc4c4e1
byrne-saw <byrnes.aw@gmail.com>
2018-10-16 18:08:30 -0500

    prep for class

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+     <h1>Interesting Peeps: {{this.people.length}}</h1>
+
+        <ul>
+          <li v-for="person in people" v-on:click="" v-if="">{{person.name}}: {{person.bio}}</li>
+        </ul>
+
+        <div>
+          Name: <input v-model="newPerson.name">
+          Bio: <input v-model="newPerson.bio">
+          <button v-on:click="addPerson()">Add person</button>
+        </div>
+
+
   </div>
 </template>
 
@@ -10,9 +22,23 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      people: [
+              {name: "Kek", bio: "The one and only", bioVisible: true},
+              {name: "Whypler", bio: "an unfortunate series of events", bioVisible: true},
+              {name: "scrod", bio: "better left unsaid", bioVisible: true}
+              ],
+      newPerson: {name: "", bio: "", bioVisible: true}
+    };
+  },
+  created: function() {},
+  methods: {
+    addPerson: function() {
+      this.people.push(this.newPerson);
+      this.newPerson = {name: "", bio: "", completed: false};
+    }
+  },
+  computed: {}
+};
 </script>
